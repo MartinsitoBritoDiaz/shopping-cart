299a04a63c731d3c813a9fba5f5c6e9c71121662
Martinsito <martinsitobd@gmail.com>
2022-08-14 16:48:52 -0400

    Sneakers were added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    sneakers: [
+      { name: 'Alcantara', price: 149.99, img: './sneakers/nike-1'},
+      { name: 'Aurora', price: 89.99, img: './sneakers/nike-2'},
+      { name: 'Spacial', price: 69.99, img: './sneakers/nike-3'},
+      { name: 'Blood', price: 120, img: './sneakers/nike-4'},
+      { name: 'Tension', price: 99.99, img: './sneakers/nike-5'},
+      { name: 'Huarache', price: 196.99, img: './sneakers/nike-6'},
+      { name: 'Waffle', price: 64.99, img: './sneakers/nike-7'},
+      { name: 'Cortez', price: 55, img: './sneakers/nike-8'}, 
+      { name: 'Blazer', price: 76.99, img: './sneakers/nike-9'},
+      { name: 'Flyknit', price: 43.99, img: './sneakers/nike-10'},
+    ]
+  }
+
   render() {
     return (
        <div>
